ca96a9b66d6d372d0d1afb9e267a085a2e62a70f
Slawek <slawomir.goszcz@schibsted.pl>
2012-11-22 17:39:26 +0100

    3

--- unityproject/Assets/server-client.js
+++ unityproject/Assets/server-client.js
@@ -52,3 +52,10 @@ function OnGUI () {
   	
 }
 }
+
+function OnConnectedToServer () {
+ // Notify our objects that the level and the network are ready
+ 	for (var go : GameObject in FindObjectsOfType(GameObject))
+  		go.SendMessage("OnNetworkLoadedLevel", 
+		SendMessageOptions.DontRequireReceiver);  
+}
